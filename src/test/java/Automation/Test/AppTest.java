8e6555d25c0f4bda92d39fcf7a2f82863e0f8d2f
HP <HP@DESKTOP-1S9RFJP>
2025-03-09 22:04:03 +0530

    First Commit

---
+++ src/test/java/Automation/Test/AppTest.java
@@ -0,0 +1,23 @@
+package Automation.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void Test()
+	{
+		ChromeOptions obj = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(obj);
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+	}
+}
